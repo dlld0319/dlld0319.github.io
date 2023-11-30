0a6c9016f6aa8a86733b63a9a0723143a662d6e0
ld122481669@126.com <ld122481669@126.com>
2023-11-30 11:38:07 +0800

    fix:bug

--- src/components/pl/pl.vue
+++ src/components/pl/pl.vue
@@ -1,5 +1,8 @@
 <template>
 	<view style="z-index: 100000;position: relative;width:100%;height: 100%;background-color: #fff;">
+		<view class="" style=" padding-top: 110px;font-size: 30px;">
+			已经在加载，如果未打开可能是网络问题
+		</view>
 		<web-view   class="wb" :src="'../../static/utter.html?title='+title"></web-view>
 	</view>
 </template>
